9c0017b3d9d11cb1ae479ac5ec1e3091dad6bb1a
BilalJ0khio <jokhiobilal.11@gmail.com>
2023-07-12 16:57:02 +0500

    nav doe

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var menuCross;
+// var menuCross;
 
-var menuContainer;
-menuContainer = document.querySelector(".menu-container");
-console.log(menuContainer);
+// var menuContainer;
+// menuContainer = document.querySelector(".menu-container");
+// console.log(menuContainer);
 
-menuContainer.addEventListener("click", function () {
-  menuContainer.classList.toggle("l");
-  menuContainer.classList.toggle("cross");
-});
+// menuContainer.addEventListener("click", function () {
+//   menuContainer.classList.toggle("l");
+//   menuContainer.classList.toggle("cross");
+// });
